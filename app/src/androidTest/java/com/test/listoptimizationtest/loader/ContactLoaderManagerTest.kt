7071d1b292f27d79048c73a871c2597d82636d2d
Jonathan <jcasagrandefilho@protonmail.com>
2019-05-18 21:02:42 +0100

    init test

---
+++ app/src/androidTest/java/com/test/listoptimizationtest/loader/ContactLoaderManagerTest.kt
@@ -0,0 +1,41 @@
+package com.test.listoptimizationtest.loader
+
+import android.support.test.InstrumentationRegistry
+import android.util.Log
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import com.test.listoptimizationtest.MainActivity
+import com.test.listoptimizationtest.model.Contact
+import org.junit.Assert.assertNotNull
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+
+class ContactLoaderManagerTest {
+	
+	@Rule
+	var activityScenarioRule: ActivityScenarioRule<MainActivity> = ActivityScenarioRule(MainActivity::class.java)
+	lateinit var contactLoader : ContactLoaderManager
+	@Before
+	fun setUp() {
+		val context = InstrumentationRegistry.getTargetContext()
+		contactLoader = ContactLoaderManager(context, object :
+			ContactLoaderManager.ContactLoaderCallback {
+			override fun loadFinished(contacts: List<Contact>) {
+				Log.d("TAG","Size:${contacts.size}")
+			}
+		})
+	}
+	
+	@Test
+	fun testCreateLoader(){
+		val cursoLoader = contactLoader.onCreateLoader(0,null)
+		assertNotNull(cursoLoader)
+	}
+	
+	@Test
+	fun testLoaderFinish(){
+		//when(contactLoader.onLoadFinished(contactLoader.onCreateLoader(0,null),null))
+		
+	}
+}
